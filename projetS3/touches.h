a1b891dcee1bb8640a6cbc978a6a959012c967ff
LeSlipCoding <djessy1998@orange.fr>
2018-11-18 19:47:57 +0100

    Correction du bug é à ç è + ajout de "touches.h"

---
+++ projetS3/touches.h
@@ -0,0 +1,22 @@
+// DIFFERENTES TOUCHES AVEC SCAN CODE
+
+//CARACTERES SPECIAUX
+#define ECHAP	9
+#define ESPER	10
+#define EACC	11
+#define DOUBLEQUOTE 12
+#define QUOTE 13
+#define LEFTPAREN	14
+#define TIRET 15
+#define EACCGRA	16
+#define UNDERSCORE	17
+#define CCEDILLE	18
+#define AACCENT	19
+
+// LETTRES
+#define Z	25
+#define Q	38
+#define S 	39
+#define D 	40
+#define E 	26
+#define F 	41
