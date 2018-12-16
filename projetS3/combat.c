722200d61e7fab8df2bea4c1bd64629e178bc3a6
djessy1998 <djessy1954@orange.fr>
2018-12-16 15:58:23 +0100

    Commentaires + petit reglage bug

--- projetS3/combat.c
+++ projetS3/combat.c
@@ -8,11 +8,13 @@ void combat(monstre *m, character *a, monde monde, input *input){
   if(m->coupPr < 0){
     m->coupPr = 500;
   }
+  //Calcule des positions x et y joueur/monstre
   int yMondeMo = (m->y - HAUTEUR_MONSTRE)/TAILLE_BLOCS;
   int xMondeMo = m->x/TAILLE_BLOCS;
   int xMondeJ = (a->xMonde + a->pos.x)/16;
   int yMondeJ = (((a->yMonde + (NBBLOCS_FENETREY*TAILLE_BLOCS - a->pos.y) - PLAYER_HEIGHT)/16));
   if((((yMondeMo - 1 >= yMondeJ - 3  && yMondeMo - 1 <= yMondeJ)) && (xMondeMo == xMondeJ))){
+    //Si le joueur n'est plus invincible (Si il ne c'est pas fait touché pendant x secondes)
     if(a->invinc > 500){
     	if(m->dernierSaut == GAUCHE){
     	  a->xMondeDouble =  a->xMondeDouble - 20;
@@ -46,6 +48,7 @@ void combat(monstre *m, character *a, monde monde, input *input){
 
 void game_over(character *a, monde monde, SDL_Surface *screen, int *inc, input *input){
   if(a->PV == 0){
+    //*inc sert à laisser l'écran sur "You were slain ..." quelques temps puis on fait réapparaitre le joueur
     if(*inc < 300){
       a->mort = 1;
       SDL_Rect posTexte;
@@ -55,12 +58,13 @@ void game_over(character *a, monde monde, SDL_Surface *screen, int *inc, input *
       SDL_Color couleurNoire = {0, 0, 0};
       posTexte.x = SCREEN_WIDTH/2 - 120;
       posTexte.y = SCREEN_HEIGHT/2;
-      motMort = TTF_RenderText_Solid (police,"You were slain ..." , couleurNoire);
+      motMort = TTF_RenderText_Blended (police,"You were slain ..." , couleurNoire);
       SDL_BlitSurface(motMort, NULL, screen, &posTexte);
       TTF_CloseFont(police);
       SDL_FreeSurface(motMort);
     }else{
       apparition_joueur(a,monde);
+      //On vide l'inventaire quand le joueur meurt
       for(int i = 0; i < 4; i++){
         for(int j = 0; j < 10; j++){
             input->data.inv[i][j].type = -1;
--- projetS3/construction.c
+++ projetS3/construction.c
@@ -10,23 +10,25 @@ void construction(monde *monde, input *input, character *a, int *choixAct){
   int jb,ib,i,j,trouve = 0;
   if(*choixAct == 3){
     if((estVide(monde->grilleInt[a->minaY][a->minaX])) && input->data.butDown == 1 && (abs(a->minaX - posJX) < LIMITEMINA && (abs(a->minaY - posJY) < LIMITEMINA || abs(a->minaY - (posJY - PLAYER_HEIGHT/TAILLE_BLOCS)) <= LIMITEMINA))){
-      monde->grilleInt[a->minaY][a->minaX] = TERRE1;
-      for(i=0;i<4;i++){
-        for(j=0;j<10;j++){
-          if(input->data.inv[i][j].type == 3){
-            jb = j;
-            ib = i;
-            trouve = 1;
-            break;
+      if(a->minaX <= posJX-1 || a->minaX >= posJX + 2){
+        monde->grilleInt[a->minaY][a->minaX] = TERRE1;
+        for(i=0;i<4;i++){
+          for(j=0;j<10;j++){
+            if(input->data.inv[i][j].type == 3){
+              jb = j;
+              ib = i;
+              trouve = 1;
+              break;
+            }
           }
+        	if(trouve == 1){
+        	 break;
+        	}
         }
-      	if(trouve == 1){
-      	 break;
-      	}
+        input->data.inv[ib][jb].type = -1;
+        input->data.inv[ib][jb].nomItem = " ";
+        *choixAct = 0;
       }
-      input->data.inv[ib][jb].type = -1;
-      input->data.inv[ib][jb].nomItem = " ";
-      *choixAct = 0;
     }
   }
 }
